4392d4dd781af9ac9a51b216724469c8b18fe00b
Shawn Shacterman <shawnshact@gmail.com>
2018-11-06 17:41:10 -0800

    changed variable val to eval (to show git functionality to team member)

--- scheme.py
+++ scheme.py
@@ -22,8 +22,8 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     elif expr.first == "define":
         if expr.second is not nil and expr.second.second is not nil:
             symbol = expr.second.first
-            val = expr.second.second.map(lambda param: scheme_eval(param, env)).first
-            return env.define(symbol, val)
+            eval = expr.second.second.map(lambda param: scheme_eval(param, env)).first
+            return env.define(symbol, eval)
         else:
             raise SchemeError("define must contain at least 2 items.")
     elif expr.first == 'quote':
